e5ec112328a6a3535c3595fe8078b1d90ca38148
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2025-04-14 19:03:54 +0200

    fixed test for new coordinates type

--- painter/lang/parser_test.go
+++ painter/lang/parser_test.go
@@ -25,7 +25,6 @@ update`
 		t.Fatalf("Очікувалося 5 операцій, але отримано %d", len(ops))
 	}
 
-	// Перевірка типів і значень
 	t.Run("check types and values", func(t *testing.T) {
 		// #0: WhiteFill
 		if _, ok := ops[0].(painter.OperationFunc); !ok {
@@ -34,7 +33,7 @@ update`
 
 		// #1: RectOperation
 		if rect, ok := ops[1].(*painter.RectOperation); ok {
-			want := &painter.RectOperation{X1: 0.1, Y1: 0.2, X2: 0.3, Y2: 0.4}
+			want := &painter.RectOperation{X1: 80, Y1: 160, X2: 240, Y2: 320}
 			if rect.X1 != want.X1 || rect.Y1 != want.Y1 || rect.X2 != want.X2 || rect.Y2 != want.Y2 {
 				t.Errorf("RectOperation має неправильні координати: %+v", rect)
 			}
@@ -44,28 +43,28 @@ update`
 
 		// #2: MoveFiguresOperation
 		if move, ok := ops[2].(*painter.MoveFiguresOperation); ok {
-			if move.X != 0.1 || move.Y != 0.1 {
+			if move.X != 80 || move.Y != 80 {
 				t.Errorf("MoveFiguresOperation має неправильні координати: %+v", move)
 			}
 			if move.Figures == nil || len(*move.Figures) != 1 {
 				t.Errorf("MoveFiguresOperation має некоректний список фігур")
 			}
 		} else {
-			t.Errorf("Очікувався MoveFiguresOperation, отримано %T", ops[3])
+			t.Errorf("Очікувався MoveFiguresOperation, отримано %T", ops[2])
 		}
 
 		// #3: FigureOperation
 		if fig, ok := ops[3].(*painter.FigureOperation); ok {
-			if fig.X != 0.5 || fig.Y != 0.5 {
+			if fig.X != 400 || fig.Y != 400 {
 				t.Errorf("FigureOperation має неправильні координати: %+v", fig)
 			}
 		} else {
-			t.Errorf("Очікувався FigureOperation, отримано %T", ops[2])
+			t.Errorf("Очікувався FigureOperation, отримано %T", ops[3])
 		}
 
+		// #4: Update
 		if reflect.TypeOf(ops[4]).String() != "painter.updateOp" {
 			t.Errorf("Очікувався updateOp, отримано %T", ops[4])
 		}
-
 	})
 }
