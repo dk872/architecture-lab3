ab8d46778df3fca772f3c33223af94abb33529e4
Dmytro Kulyk <143122117+dk872@users.noreply.github.com>
2025-04-18 11:56:05 +0300

    Improve the script

--- scripts/green.sh
+++ scripts/green.sh
@@ -2,7 +2,8 @@
 
 curl -X POST http://localhost:17000 -d "reset"
 curl -X POST http://localhost:17000 -d "white"
-curl -X POST http://localhost:17000 -d "update"
+curl -X POST http://localhost:17000 -d "bgrect 0.18 0.18 0.82 0.82"
+curl -X POST http://localhost:17000 -d "figure 0.5 0.5"
 curl -X POST http://localhost:17000 -d "green"
-curl -X POST http://localhost:17000 -d "bgrect 0.25 0.25 0.75 0.75"
+curl -X POST http://localhost:17000 -d "figure 0.55 0.55"
 curl -X POST http://localhost:17000 -d "update"
